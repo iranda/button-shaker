c765088244f060c557f533637e374eabf7e76492
Daria Sirous <daria.sirous@gmail.com>
2018-07-09 22:42:20 -0400

    Adds page layout

---
+++ src/components/ButtonSetup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class ButtonSetup extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default ButtonSetup;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
-import App from './components/App';
+import ButtonSetup from './components/ButtonSetup';
+import DefaultLayout from './layouts/DefaultLayout';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <DefaultLayout path='/' component={ButtonSetup} />
+    </Switch>
+  </Router>,
+  document.getElementById('root'));
